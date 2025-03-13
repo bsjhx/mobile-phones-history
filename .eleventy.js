9468cf806db577027ed0196687fb42d81c25c066
Michal Rakoczy <rakoczymichal@hotmail.com>
2025-03-13 20:11:26 +0100

    Added collections

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,24 @@
+const sortByDisplayOrder = require('./src/utils/sort-by-display-order.js');
+
 module.exports = (config) => {
     config.addPassthroughCopy('./src/images/');
+
+    // Returns work items, sorted by display order
+    config.addCollection('work', (collection) => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
+    });
+
+    // Returns work items, sorted by display order then filtered by featured
+    config.addCollection('featuredWork', (collection) => {
+        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
+            (x) => x.data.featured,
+        );
+    });
+
+    config.addCollection('phone', function (collection) {
+        return collection.getFilteredByGlob('./src/phone/*.md');
+    });
+
     return {
         markdownTemplateEngine: 'njk',
         dataTemplateEngine: 'njk',
