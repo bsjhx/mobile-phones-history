4ecbb7b232a05341396438f9df2f62fbea78494f
bsjhx <rakoczymichal@hotmail.com>
2025-03-19 19:52:20 +0100

    Added rest things

--- .eleventy.js
+++ .eleventy.js
@@ -4,20 +4,10 @@ require('dotenv').config();
 module.exports = (config) => {
     config.addPassthroughCopy('./src/images/');
 
-    // Returns work items, sorted by display order
-    config.addCollection('work', (collection) => {
-        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md'));
-    });
-
-    // Returns work items, sorted by display order then filtered by featured
-    config.addCollection('featuredWork', (collection) => {
-        return sortByDisplayOrder(collection.getFilteredByGlob('./src/work/*.md')).filter(
-            (x) => x.data.featured,
-        );
-    });
-
     config.addCollection('phone', function (collection) {
-        return collection.getFilteredByGlob('./src/phone/*.md');
+        console.log(collection.getFilteredByGlob('./src/phone/*.md'))
+        return collection.getFilteredByGlob('./src/phone/*.md')
+            .sort((a, b) => b.data.id - a.data.id);
     });
 
     const isProd = process.env.NODE_ENV === 'production';
